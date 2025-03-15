d1638d0a60c354ad87e0ae669949e1e5cd7a763e
Indu61 <indugadagi61@gmail.com>
2025-03-15 23:26:40 +0530

    added cors config

--- src/main/java/com/expense_tracker/expense_tracker/config/SecurityConfig.java
+++ src/main/java/com/expense_tracker/expense_tracker/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
 @Bean
 public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:4200","https://67d45b11d6665d0008f5b1e9--trackyourexpense1.netlify.app/")); 
+    configuration.setAllowedOrigins(List.of("http://localhost:4200","https://trackyourexpense1.netlify.app")); 
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); 
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true); 
